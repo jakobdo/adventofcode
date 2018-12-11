6c302399b1c43a3b06922e55f40fd9287d7d596f
Jakob Damgaard Møller <jakobdo@gmail.com>
2018-12-11 16:05:19 +0100

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,4 +1,5 @@
+start = 0
 with open("day1.txt") as f:
         for cnt, line in enumerate(f):
-                new_line = line.strip()
-                print(f"Line: {cnt} : {new_line}")
+                start += int(line.strip())
+print(start)
